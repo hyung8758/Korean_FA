fc38fcf2c375788904639a8bbb0ecfa6dd879132
hyung8758 <hyung8758@gmail.com>
2023-08-13 02:06:24 +0900

    add up 2 handlers in serverhanler.py

--- src/handlers/ServerHandler.py
+++ src/handlers/ServerHandler.py
@@ -12,7 +12,7 @@ import tornado.web
 from src.handlers.MainHandler import MainHandler
 CURRENT_PATH = os.path.dirname(os.path.realpath(__file__))
 
-class HttpServerHandler(tornado.web.RequestHandler):
+class MainHanlder(tornado.web.RequestHandler):
     def get(self):
         self.render("../web/html/main.html")
         
@@ -21,11 +21,20 @@ class HttpServerHandler(tornado.web.RequestHandler):
         MainHandler.process(data)
         self.write(f"Received POST data: {data}")
         
+class resultHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("../web/html/result.html")
+
+class uploadHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("../web/html/upload.html")
 
 class App(tornado.web.Application):
     def __init__(self):
         handlers = [
-            (r'/', HttpServerHandler),
+            (r'/', MainHanlder),
+            (r'/result', resultHandler),
+            (r'/upload', uploadHandler),
             (r'/html/(.*)', tornado.web.StaticFileHandler, {"path": os.path.join(CURRENT_PATH, "../web/html/")}),
             (r'/css/(.*)', tornado.web.StaticFileHandler, {"path": os.path.join(CURRENT_PATH, "../web/css/")}),
             (r'/js/(.*)', tornado.web.StaticFileHandler, {"path": os.path.join(CURRENT_PATH, "../web/js/")}),
