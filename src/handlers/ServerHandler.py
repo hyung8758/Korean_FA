9a7383899178ec562ef875ed6d3bfa0c681794d9
hyung8758 <hyung8758@gmail.com>
2023-08-17 01:22:06 +0900

    add send data dict variable for a client.

--- src/handlers/ServerHandler.py
+++ src/handlers/ServerHandler.py
@@ -6,6 +6,8 @@ Hyungwon Yang
 """
 
 import os, sys, io
+import shutil
+import copy
 import logging
 import json
 import tornado.web
@@ -15,7 +17,11 @@ from src.handlers.DataHandler import FAhistory
 from src.utils.DateUtils import DateUtils
 
 CURRENT_PATH = os.path.dirname(os.path.realpath(__file__))
-
+sendData = dict(
+    message=None,
+    history=None,
+    success=True,
+)
 class MainHanlder(tornado.web.RequestHandler):
     def get(self):
         self.render("../web/html/main.html")
@@ -29,15 +35,24 @@ class resultHandler(tornado.web.RequestHandler):
         self.render("../web/html/result.html")
         
     def post(self):
+        curSendData = copy.deepcopy(sendData)
         # extract information.
         data = json.loads(self.request.body)
         print("received: {}".format(data))
         cmd = data.get("command", None)
         # read history
         historyLog = self.fahistory.read_history()
+        # follow the command.
+        if cmd == "remove":
+            pass
+        elif cmd == "download":
+            pass
+        else:
+            pass
         print("history log: {}".format(historyLog))
         self.set_header("Content-Type", "application/json")
-        self.write(json.dumps({"success":True, "history":historyLog}))
+        curSendData["history"] = historyLog
+        self.write(json.dumps(curSendData))
 
 class uploadHandler(tornado.web.RequestHandler):
     def get(self):
@@ -46,8 +61,9 @@ class uploadHandler(tornado.web.RequestHandler):
     def post(self):
         # 파일 다운로드 관리.
         try:
+            curSendData = copy.deepcopy(sendData)
             file_list = dict()
-            print("how manay file?: {}".format(self.request.files.keys()))
+            print("file number: {}".format(self.request.files.keys()))
             for each_key in self.request.files.keys():
                 file_list[each_key] = self.request.files[each_key]
             # print("data:{}".format(file_list))
@@ -64,14 +80,19 @@ class uploadHandler(tornado.web.RequestHandler):
                     else:
                         logging.info("Unknown tpye: {}".format(k))
             # save it to history.
-            
+            curSendData["message"] = "Upload successful"
             self.set_status(200)
-            self.write(json.dumps({"message": "Upload successful", "success": True}))
-            print("successful!!")
+            print("successful!")
         except Exception as e:
-            # self.set_status(400)
-            self.write(json.dumps({"error":str(e)}))
-            print("failed!!: {}".format(e))
+            self.set_status(400)
+            curSendData["error"] = e
+            curSendData["success"] = False
+            print("failed!: {}".format(e))
+            # remove previous generated data dir.
+            if os.path.exists(dataPath):
+                shutil.rmtree(dataPath)
+        finally:
+            self.write(json.dumps(curSendData))
 
 class App(tornado.web.Application):
     def __init__(self):
