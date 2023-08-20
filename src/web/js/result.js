7fb74ac08f03fd720d5f3a6f54e174971c5eb377
hyung8758 <hyung8758@gmail.com>
2023-08-20 23:53:06 +0900

    instead of using hard-coded delete and download button they are written in the each function. DownloadResult and stopProcess functions are completed

--- src/web/js/result.js
+++ src/web/js/result.js
@@ -1,15 +1,3 @@
-const BTN_DEL = `
-<button class="delete" onclick="stopProcess(this);">
-<svg width="16" height="16" fill="grey" class="bi bi-trash3-fill" viewBox="0 0 16 16">
-  <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z"/>
-</svg>
-</button>`
-const BTN_DOWNLOAD = `<button class="download" onclick="downloadResult(this);">
-<svg width="16" height="16" fill="grey" class="bi bi-file-earmark-arrow-down-fill" viewBox="0 0 16 16">
-    <path d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0zM9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1zm-1 4v3.793l1.146-1.147a.5.5 0 0 1 .708.708l-2 2a.5.5 0 0 1-.708 0l-2-2a.5.5 0 0 1 .708-.708L7.5 11.293V7.5a.5.5 0 0 1 1 0z"/>
-</svg>
-</button>`
-
 
 const getData = () => {
     const url = "/result"
@@ -25,21 +13,15 @@ const getData = () => {
     })
     .then(response => response.json())
     .then(data => {
-        console.log("data: ",data)
         if (data.success === true) {
             const history = data.history;
-            console.log("data history length: ",history.length);
             table.clear();
-            console.log("table clear: ",history);
             for(let i=0; i<history.length; i++) {
-                console.log("adding table : ",history[i]);
                 table.row.add(history[i]);
             }
-            console.log("data drawing?: started")
             table.draw();
-            console.log("data drawing?: finished")
         } else {
-          console.log('URL: ', url);
+            alert("ERROR getdata: ", data.error);
         }
     })
     .catch(error => {
@@ -87,31 +69,39 @@ $(document).ready(function () {
                 targets: 5,
                 width: "30px",
                 render: function ( data, type, row ) {
-                    // // console.log("data.progress: ", data)
-                    // const isButtonEnabled = data.progress === "100%";
-                    
-                    // const DownloadButton = document.createElement("button");
-                    // DownloadButton.type = "button";
-                    // DownloadButton.className = "download";
-                    // DownloadButton.innerHTML = '<svg width="16" height="16" fill="grey" class="bi bi-file-earmark-arrow-down-fill" viewBox="0 0 16 16">\
-                    //     <path d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0zM9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1zm-1 4v3.793l1.146-1.147a.5.5 0 0 1 .708.708l-2 2a.5.5 0 0 1-.708 0l-2-2a.5.5 0 0 1 .708-.708L7.5 11.293V7.5a.5.5 0 0 1 1 0z"/>\
-                    //     </svg>';
+                    // values are stored in row.
+                    const isButtonEnabled = row.progress === "100%";
+                    const imageOpacity = isButtonEnabled ? 1 : 0.5;
 
-                    // if (!isButtonEnabled) {
-                    //     DownloadButton.disabled = true;
-                    // } else {
-                    //     DownloadButton.addEventListener('click', function (event) { 
-                    //         downloadResult(event);
-                    //     });
-                    // }
-                    return BTN_DOWNLOAD;
+                    const DownloadButton = document.createElement("button");
+                    DownloadButton.type = "button";
+                    DownloadButton.className = "download";
+                    DownloadButton.innerHTML = `<svg width="16" height="16" fill="grey" class="bi bi-file-earmark-arrow-down-fill" viewBox="0 0 16 16" style="opacity: ${imageOpacity};">
+                        <path d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0zM9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1zm-1 4v3.793l1.146-1.147a.5.5 0 0 1 .708.708l-2 2a.5.5 0 0 1-.708 0l-2-2a.5.5 0 0 1 .708-.708L7.5 11.293V7.5a.5.5 0 0 1 1 0z"/>
+                        </svg>`;
+
+                    if (!isButtonEnabled) {
+                        DownloadButton.disabled = true;
+                    } else {
+                        DownloadButton.setAttribute('onclick', `downloadResult(${JSON.stringify(row)})`)
+                    }
+                    return DownloadButton.outerHTML;
                 }
             },
             {
                 targets: 6,
                 width: "30px",
                 render: function ( data, type, row ) {
-                    return BTN_DEL;
+
+                    const DeleteButton = document.createElement("button");
+                    DeleteButton.type = "button";
+                    DeleteButton.className = "delete";
+                    DeleteButton.innerHTML = `<svg width="16" height="16" fill="grey" class="bi bi-trash3-fill" viewBox="0 0 16 16">
+                        <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z"/>
+                        </svg>`;
+                    
+                    DeleteButton.setAttribute('onclick', `stopProcess(${JSON.stringify(row)})`)
+                    return DeleteButton.outerHTML;
                     },
 	        },
         ],
@@ -125,22 +115,18 @@ $(document).ready(function () {
     getData();
 });
 
-const downloadResult = (event) => {
+const downloadResult = (rowValue) => {
     const url = "result"
-    const table = $('#resultTable').DataTable();
-    
-    const rowitem = $($(event).parent()[0]).parent()[0];
-    if(!rowitem) return;
-    const row = table.row(rowitem);
-    const date = row.data()['date'];
-    const language = row.data()['langauge'];
+    if(!rowValue) return;
+    const date = rowValue['date'];
+    const language = rowValue['language'];
     
     const result = confirm(`${date}을 다운로드합니다.`);
     if(!result) return;
 
     const sendData = {"command":"download", 
                         "date": date,
-                        "langauge": language};
+                        "language": language};
     fetch(url, {
         method: 'POST',
         headers: {
@@ -148,40 +134,33 @@ const downloadResult = (event) => {
             },
         body: JSON.stringify(sendData)
     })
-    .then(response => response.json())
-    .then(data => {
-        console.log("data: ", data)
-        if (data.success === true) {
-            const history = data.history;
-            table.clear();
-            for(let i=0; i<history.length; i++)
-                table.row.add(history[i]);
-            table.draw();
-        } else {
-            console.log('URL: ', url);
-        }
+    .then(response => response.blob())
+    .then(blob => {
+        const downloadLink = document.createElement('a');
+        const url = window.URL.createObjectURL(blob);
+        downloadLink.href = url;
+        downloadLink.download = `${date}-${language}.zip`;
+        downloadLink.click();
+        window.URL.revokeObjectURL(url);
     })
     .catch(error => {
         alert("ERROR downloader: ", error);
     });
 }
 
-const stopProcess = (event) => {
+const stopProcess = (rowValue) => {
     const url = "result"
-    const table = $('#resultTable').DataTable();
     
-    const rowitem = $($(event).parent()[0]).parent()[0];
-    if(!rowitem) return;
-    const row = table.row(rowitem);
-    const date = row.data()['date'];
-    const language = row.data()['langauge'];
+    if(!rowValue) return;
+    const date = rowValue['date'];
+    const language = rowValue['language'];
     
     const result = confirm(`${date}을 삭제합니다.`);
     if(!result) return;
     
     const sendData = {"command":"remove", 
                         "date": date,
-                        "langauge": language};
+                        "language": language};
     fetch(url, {
         method: 'POST',
         headers: {
@@ -193,13 +172,14 @@ const stopProcess = (event) => {
     .then(data => {
         console.log("data: ", data)
         if (data.success === true) {
+            const table = $('#resultTable').DataTable();
             const history = data.history;
             table.clear();
             for(let i=0; i<history.length; i++)
                 table.row.add(history[i]);
             table.draw();
         } else {
-          console.log('URL: ', url);
+            alert("ERROR stopprocess: ", data.error);
         }
     })
     .catch(error => {
