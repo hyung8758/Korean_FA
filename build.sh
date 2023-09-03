f5df98a86db48928d1d9440b826de220ba323c84
hyung8758 <hyung8758@gmail.com>
2023-09-03 17:41:03 +0900

    this build.sh for a user

--- build.sh
+++ build.sh
@@ -5,27 +5,33 @@
 # 23.09.03
 
 
-IMAGE_NAME=korean_fa_app
+IMAGE_NAME=koreanfa
 DOCKER_PATH=./docker/dockerfile
 SOURCE_PATH=. # only if you are in Korean_FA directory.
 
 # please adjust parameters based on your OS setting.
 USE_BUILDX=false # true if use buildx
 USE_SUDO=false # true if needs to use sudo
-USE_CACHE=true # false if you want to rebuild it from the beginning.
+USE_CACHE=false # false if you want to rebuild it from the beginning.
 
-option=
-sudo_cmd=
+option=""
+docker_cmd=
 
+# sudo user.
 if $USE_SUDO; then
-    sudo_cmd=sudo; fi
-if ! $USE_CACHE; then
-    option+="--no-cache"; fi
-
+    docker_cmd+="sudo "; fi
+# use buildx?
 if $USE_BUILDX; then
-    # buildx
-    $sudo_cmd docker buildx build  $option -t $IMAGE_NAME -f $DOCKER_PATH $SOURCE_PATH
+    docker_cmd+="docker buildx build "
 else
-    # default build command
-    $sudo_cmd docker build $option -t $IMAGE_NAME $DOCKER_PATH $SOURCE_PATH
-fi
+    docker_cmd+="docker build "; fi
+# use chahe?
+if ! $USE_CACHE; then
+    docker_cmd+="--no-cache "; fi
+# add option if exists.
+docker_cmd+=$option" "
+# add image name, dockerfile path, and path directory.
+docker_cmd+="-t $IMAGE_NAME -f $DOCKER_PATH $SOURCE_PATH"
+# build an image.
+echo "build command: $docker_cmd"
+$docker_cmd
